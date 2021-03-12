75ad156dc27a23981549522114a736375d1120c0
Ve1orum <45082368+Ve1orum@users.noreply.github.com>
2021-03-12 12:10:29 +0200

    Add files via upload

---
+++ tickets-shop/src/main/java/com/jkelly/tickets/ticketsshop/TicketsShopApplication.java
@@ -0,0 +1,13 @@
+package com.jkelly.tickets.ticketsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsShopApplication.class, args);
+	}
+
+}
